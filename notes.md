71ac0397d229ef0772bdb0ab74d97b5004587842
Roland Levy <rolandjlevy@gmail.com>
2020-11-25 22:29:14 +0000

    Improved layout and functionality

--- notes.md
+++ notes.md
@@ -5,3 +5,4 @@
 **Idea started with this demo that uses detail and summary tags**
 - https://twitter.com/stackblitz/status/1331640548352901123?s=09
 - https://stackblitz.com/edit/dialog-details?file=index.js
+- See old folder for original code. The details / summary toggle doesn't work
--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const $ = (selector) => document.querySelector(selector);
 
 $('button.open').addEventListener("click", e => {
   if (!$('dialog').open) {
+    $('.container').classList.add('active');
     $('dialog').showModal();
   }
+});
+
+$('dialog').addEventListener("submit", e => {
+  console.log('cancel')
+  $('.container').classList.remove('active');
 });
